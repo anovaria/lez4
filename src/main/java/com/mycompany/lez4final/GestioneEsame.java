aad0cc3c66c0d025c0fc34a95302ca0142a48ee3
tss <tss@debian>
2019-11-11 12:31:34 +0100

    vers0

---
+++ src/main/java/com/mycompany/lez4final/GestioneEsame.java
@@ -0,0 +1,66 @@
+package com.mycompany.lez4final;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class GestioneEsame {
+//Attributi della classe
+
+    static String nomeEsame;
+    static int listaVoti[];
+    static String[] listaCognomi;
+    static String[] listaNomi;
+
+    public static void main(String[] args) {
+        // fase 1
+        preparaEsame();
+        // fase 2
+        sessioneEsame();
+        // fase 3
+        correzioneEsami();
+
+        // fase 4
+        visTabellone();
+    }
+
+    static void preparaEsame() {
+        //inizializza l' esame ovvero il registro
+        nomeEsame = JOptionPane.showInputDialog("nome esame");
+        int quanti = Utils.AskInt("Numero max alunni:", 1, 100);
+        listaCognomi = new String[quanti];
+        listaNomi = new String[quanti];
+        listaVoti = new int[quanti];
+
+    }
+
+    private static void sessioneEsame() {
+        //carico il registro
+        for (int i = 0; i < listaCognomi.length; i++) {
+            listaCognomi[i] = JOptionPane.showInputDialog("cognome: " + (i + 1) + " di " + listaCognomi.length);
+            listaNomi[i] = JOptionPane.showInputDialog("nome:");
+
+        }
+
+    }
+
+    private static void correzioneEsami() {
+//voti
+        for (int i = 0; i < listaCognomi.length; i++) {
+            String msg = "Inserisci voto di" + listaNomi[i] + listaCognomi[i];
+            listaVoti[i] = Utils.AskInt(msg, 0, 100);
+
+        }
+    }
+
+    private static void visTabellone() {
+        String msg = "Nome Esame: "+nomeEsame+"\n";
+        for (int i = 0; i < listaCognomi.length; i++) {
+            msg = msg +  listaNomi[i] + " " + listaCognomi[i] + " voto: " + listaVoti[i]+"\n";
+        }
+        JOptionPane.showInternalMessageDialog(null, msg);
+
+    }
+}
---
+++ src/main/java/com/mycompany/lez4final/Utils.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lez4final;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class Utils {
+
+    static int AskInt(String ask) {
+        String strInt;
+        int ris = 0;
+        boolean skif = true;
+        while (skif) {
+            strInt = JOptionPane.showInputDialog(ask);
+            try {
+                ris = Integer.parseInt(strInt);
+                skif = false;
+
+            } catch (NumberFormatException err) {
+                JOptionPane.showMessageDialog(null, err);
+
+            } catch (Exception err) {
+                JOptionPane.showMessageDialog(null, err);
+
+            }
+
+        }
+        return ris;
+    }
+
+    static int AskInt(String ask, int min, int max) {
+        String strInt;
+        int ris = 0;
+        boolean skif = true;
+        while (skif) {
+            strInt = JOptionPane.showInputDialog(ask + "minimo" + min + "masimo" + max);
+            try {
+                ris = Integer.parseInt(strInt);
+                if (ris >= min && ris <= max) {
+                    skif = false;
+                } else {
+                    JOptionPane.showMessageDialog(null, "numero non corretto!!");
+                }
+
+            } catch (NumberFormatException err) {
+                JOptionPane.showMessageDialog(null, err);
+
+            } catch (Exception err) {
+                JOptionPane.showMessageDialog(null, err);
+
+            }
+
+        }
+        return ris;
+    }
+
+    static float AskFloat(String ask, int min, int max) {
+        String strFloat;
+        float ris = 0;
+        boolean skif = true;
+        while (skif) {
+            strFloat = JOptionPane.showInputDialog(ask + "minimo" + min + "masimo" + max);
+            try {
+                ris = Float.parseFloat(strFloat);
+                if (ris >= min && ris <= max) {
+                    skif = false;
+                } else {
+                    JOptionPane.showMessageDialog(null, "numero non corretto!!");
+                }
+
+            } catch (NumberFormatException err) {
+                JOptionPane.showMessageDialog(null, err);
+
+            } catch (Exception err) {
+                JOptionPane.showMessageDialog(null, err);
+
+            }
+
+        }
+        return ris;
+    }
+}
